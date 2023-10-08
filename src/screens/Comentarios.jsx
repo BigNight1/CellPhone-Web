fc6fe7a101405bd7d8a64afe864aadc5f0cbae8f
Edu Walter Armas Aliaga <edu_armas11@hotmail.com>
2023-10-08 16:55:45 -0500

    faceeee

--- src/screens/Comentarios.jsx
+++ src/screens/Comentarios.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Comentarios = () => {
   return (
-    <div>
-      <div class="fb-comments" data-href="https://cell-phone-web.vercel.app/" data-width="100%" data-numposts="5"></div>
-    </div>
+    <div class="fb-comments" data-href="https://cell-phone-web.vercel.app/" data-width="100%" data-numposts="5"></div>
   );
 };
 
